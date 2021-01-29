2090b35ac734111d6e7aa8ca3f6b313eeb10c1d7
Vladimir <freshcode.yt@gmail.com>
2021-01-29 16:59:12 +0200

    Add attr to createElement

--- assets/js/index.js
+++ assets/js/index.js
@@ -99,7 +99,6 @@ function stringToColour(str) {
   return colour;
 }
 
-
 /* 
 
   LIB
@@ -110,13 +109,36 @@ function stringToColour(str) {
  * @param {string} type
  * @param {object} options
  * @param {string[]} options.classNames
+ * @param {object} options.attributes
  * @param {function} options.onClick
  * @param {HTMLElement[]} children
  */
-function createElement(type, { classNames, onClick }, children) {
+function createElement(type, { classNames, onClick, attributes }, children) {
+  /*   
+  EXAMPLE OF ATTRIBUTES OBJECT
+  const attr = {
+    src: 'https://www.link.com',
+    alt: 'descr',
+  };
+   */
   const elem = document.createElement(type);
   elem.classList.add(...classNames);
   elem.onclick = onClick;
+
+  for (const [attrName, attrValue] of Object.entries(attributes)) {
+    elem.setAttribute(attrName, attrValue);
+  }
+
   elem.append(...children);
   return elem;
 }
+/* 
+ПРИМЕР ВЫЗОВА ФУНКЦИИ
+const img = createElement('img', {
+  attributes: {
+    src: 'src',
+    alt: '',
+    title: '',
+    id:'1',
+  },
+}); */
