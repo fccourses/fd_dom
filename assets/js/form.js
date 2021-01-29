244596030410557c4671e0bb7defd67882637a22
Vladimir <freshcode.yt@gmail.com>
2021-01-29 14:27:21 +0200

    Add task solution partially

--- assets/js/form.js
+++ assets/js/form.js
@@ -3,21 +3,51 @@
 const state = [];
 
 const form = document.getElementById('root-form');
+const list = document.getElementById('root-list');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const {
     target,
-    target: { email: emailInput },
+    target: {
+      todo: { value: rawInputValue },
+    },
   } = e;
 
-  console.dir(emailInput.value); // form
+  /* Если значение инпута пустое - ничего не делать */
+  const inputValue = rawInputValue.trim();
 
+  if (inputValue) {
+    /* Пушить в массив значение input'а при сабмите формы */
+    state.push(inputValue);
+    const li = createListItem(inputValue);
+    list.append(li);
+  }
   target.reset();
 });
 
-/* Пушить в массив значение input'а при сабмите формы */
 /* Рендерить на странице то, что было введено в форму */
-/* Если значение инпута пустое - ничего не делать */
+function createListItem(inputValue) {
+  const li = document.createElement('li');
+  const liContent = document.createTextNode(inputValue);
+
+  const btn = createButton();
+  li.append(liContent, btn);
+  return li;
+}
+
+function createButton() {
+  const btn = document.createElement('button');
+  btn.textContent = 'X';
+
+  btn.addEventListener('click', deleteHandler);
+
+  return btn;
+}
+
+function deleteHandler(event) {
+  /* magic to delete element */
+}
+
 /* Кнопка удаления у li'шки - удаляет элемент со стрн и 
     его значение из массива */
