264f9cd88e6bf9580e9a721f5a27dddce97dfe96
Vladimir <freshcode.yt@gmail.com>
2021-02-02 14:37:01 +0200

    Add contacts example solution

--- assets/js/index.js
+++ assets/js/index.js
@@ -141,4 +141,5 @@ const img = createElement('img', {
     title: '',
     id:'1',
   },
-}); */
+}); 
+*/
--- assets/js/solution.js
+++ assets/js/solution.js
@@ -1,2 +1,27 @@
+/* 
 new URL('https://www.facebook.com/DwayneJohnson'); // {hostname}
 new Map().set('www.facebook.com', 'src to fb icon'); // key - hostname
+ */
+
+const supportedSocialNetworks = new Map()
+  .set('www.facebook.com', 'fa fa-facebook')
+  .set('twitter.com', 'fa fa-twitter')
+  .set('www.instagram.com', 'fa fa-instagram');
+
+const [icons] = responseData.map((user) => createIcons(user.contacts));
+document.body.append(...icons);
+
+/**
+ *
+ * @param {Array<string>} contacts
+ */
+function createIcons(contacts) {
+  return contacts.map((contact) => {
+    const { hostname } = new URL(contact);
+    const cssClasses = supportedSocialNetworks.get(hostname);
+    const a = document.createElement('a');
+    a.setAttribute('href', contact);
+    a.setAttribute('class', cssClasses);
+    return a;
+  });
+}
